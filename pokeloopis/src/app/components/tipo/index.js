4e7dca6a7f8d61582b045da93d6856fb5b3bd8b3
lyzzandro <lyzzandro91@gmail.com>
2023-09-12 21:04:39 -0300

    removendo estilização

---
+++ pokeloopis/src/app/components/tipo/index.js
@@ -0,0 +1,9 @@
+export default function Tipo() {
+  return (
+    <>
+      <p>Olá</p>
+      <p>Olá</p>
+      <p>Olá</p>
+    </>
+  );
+}
--- pokeloopis/src/app/page.js
+++ pokeloopis/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Tipo from "./components/tipo";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>Pokeloopis</p>
+      <Tipo />
     </main>
   );
 }
